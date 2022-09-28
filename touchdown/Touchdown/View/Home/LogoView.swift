cb3a5805157859a76066d4569b6a0a0d009694dc
Umberto Mathias Iagushi Ferreira <umberto.mif@gmail.com>
2022-09-28 22:23:47 +0900

    Added the Navigation bar.

---
+++ touchdown/Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,35 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Umberto M. I. Ferreira on 2022/09/28.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
