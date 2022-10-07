96ebc6f948adcc75dcaf16eb9d48bce5ed111be9
Umberto Mathias Iagushi Ferreira <umberto.mif@gmail.com>
2022-10-07 23:04:38 +0900

    Added brand view.

--- touchdown/Touchdown/App/ContentView.swift
+++ touchdown/Touchdown/App/ContentView.swift
@@ -32,7 +32,6 @@ struct ContentView: View {
                         CategoryGridView()
                         
                         TitleView(title: "Helmets")
-                        
                         LazyVGrid(columns: gridLayout, spacing: 15, content: {
                             ForEach(products) { product in
                                 ProductItemView(product: product)
@@ -40,6 +39,8 @@ struct ContentView: View {
                         }) //: LazyVGrid
                         .padding(15)
         
+                        TitleView(title: "Brands")
+                        BrandGridView()
                         
                         FooterView()
                             .padding(.horizontal)
---
+++ touchdown/Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Umberto M. I. Ferreira on 2022/10/07.
+//
+
+import Foundation
+
+struct Brand: Codable,Identifiable {
+    let id: Int
+    let image: String    
+}
